ed912e9e89d0ab4478c38554517e985f279cfc83
Gaurab Paul <lorefnon@gmx.com>
2012-09-19 12:29:38 -0400

    Add a real test case : wipe out the stub currently in place

--- tests/spec/dom_sculptor_spec.js
+++ tests/spec/dom_sculptor_spec.js
@@ -1,6 +1,16 @@
-describe("Nothing", function(){
-    it("should evaluate to true", function(){
-        expect(true).toEqual(true);
-        expect(false).toBeTruthy();
+describe("dom_sculptor", function(){
+    it("should ensure that view is constructed before it is rendered", function(){
+        var tvar = 10;
+        var View1 = Backbone.View.extend(Sculptor).extend({
+            construct: function(){
+                tvar = 20;
+                Sculptor.construct.apply(this, arguments);
+            }
+        });
+        v = new View1;
+        v.model = new Backbone.Model({name: "Lorefnon"});
+        v.render();
+        expect(tvar).toBe(20);
+        
     });
 });
